4f0f15ccecaced4e6141c9e3adc4b4a7d17ff3be
Charles Newey <charlie@assemblyco.de>
2015-12-08 20:20:25 +0000

    Text/image replacement as well as node deletion

--- killdashian/src/js/filter.js
+++ killdashian/src/js/filter.js
@@ -1,8 +1,8 @@
 var eliminatedElements = 0;
 
-var dislike = function (node) {
-  var content = node.href + node.nodeValue + node.value;
-  if (content.match(/(kardashian|kim.*kanye)/i)) {
+var re = /(.*(kim|khloe|kourtney).*)?(kardashian)/ig;
+var dislike = function (content) {
+  if (content && content.match(re)) {
     eliminatedElements += 1;
     return true;
   }
@@ -10,20 +10,30 @@ var dislike = function (node) {
 };
 
 var terminateWithPrejudice = function (node) {
-  if (node.parentNode.parentNode) {
-    node.parentNode.parentNode.removeChild(node.parentNode);
-  } else {
-    node.parentNode.removeChild(node);
-  }
+  node.parentNode.removeChild(node);
 };
 
 var walk = function (node) {
   // modified from "cloud-to-butt" extension
   if (node) {
     var child, next;
-
     switch (node.nodeType) {
       case 1: // element
+        var tagName = node.tagName.toLowerCase();
+        if (node.children.length === 0) {
+          if (tagName === "img") {
+            if (dislike(node.src) || dislike(node.alt)) {
+              var w = node.clientWidth, h = node.clientHeight;
+              if (w > 50 && h > 50) {
+                node.src = "https://www.placecage.com/c/" + w + "/" + h;
+                break;
+              }
+            }
+          } else if (tagName === "p" || tagName === "a" || tagName[0] === "h") {
+            node.innerHTML = node.innerHTML.replace(re, "Nicholas Cage");
+            break;
+          }
+        }
       case 9: // document
       case 11: // document fragment
         child = node.firstChild;
@@ -35,7 +45,7 @@ var walk = function (node) {
         break;
       case 3: // text node
         if (node.parentElement.tagName.toLowerCase() !== "script") {
-          if (dislike(node)) {
+          if (dislike(node.nodeValue)) {
             terminateWithPrejudice(node);
           }
         }
@@ -43,7 +53,6 @@ var walk = function (node) {
     }
   }
 };
-
 walk(document.body);
 
 if (eliminatedElements > 0) {
